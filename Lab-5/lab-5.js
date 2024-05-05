884731d70e774d496d98152c99d0c3845d3a72fa
Anoj Kumar Shrestha <anojbestchoices@gmail.com>
2024-05-05 12:52:53 -0500

    Completed Lab-6

---
+++ Lab-5/lab-5.js
@@ -0,0 +1,82 @@
+    //Lab-5 Question-No 1 Create a function using function declaration named sum with one parameter of Array type, the
+            // returned result is the sum of all elements which are greater than 20.
+            
+            function sum(myArr) {
+                let sum = 0;
+                for (let index = 0; index < myArr.length; index++) {
+                    sum += myArr[index];
+                }
+                return sum;
+            }
+            console.log("The sum is : " + sum([1, 2, 3, 4, 5,10]));
+            
+            // Output: The sum is : 25
+            
+            
+            //============================================================================================================================
+            // 2. Create a function using function expression named getNewArray with one parameter of String
+            // Array, return a new array which contains all string, length is greater than and equal to 5, and
+            // contains letter ‘a’.
+            
+            const getNewArray = function (strArray) {
+                let expArray = [];
+                for (let index = 0; index < strArray.length; index++) {
+                    const element = strArray[index];
+                    if (element.length >= 5 && element.indexOf('a') !== -1) {
+                        expArray.push(element);
+                    }
+                }
+                return expArray;
+            }
+            console.log(getNewArray(['Anoj Kumar', 'Akshada', 'Prof Tina', 'Prakriti', 'Shanta', 'Tulips']));
+            
+            // Output: [ 'Anoj Kumar', 'Akshada', 'Prof Tina', 'Prakriti', 'Shanta']
+            
+            
+            //===============================================================================================================================
+            
+            
+            // 3. Implement an arrow function with feature below:
+            // concat('hi', [1,2,3], ['Hello','world']) -> return result: ['h', 'i', 1,2,3, 'Hello','world']
+            
+            const concat = (inputString, numberArray, stringArray) => {
+                let returnArr = [...inputString, ...numberArray, ...stringArray];
+                return returnArr;
+            }
+            console.log(concat('hi', [1, 2, 3], ['Hello', 'world']));
+            
+            //Output: [ 'h', 'i', 1, 2, 3, 'Hello', 'world' ]
+            
+            
+            //=================================================================================================================================
+            
+            
+            // 4.  For the given students array below, compute the average grade of all students who took cs303 course
+            //  which returns an object which key is students' names, values is the average.
+            
+            const students = [
+            
+                { name: 'Quincy', grades: [99, 88], courses: ['cs301', 'cs303'] },
+            
+                { name: 'Jason', grades: [29, 38], courses: ['cs201', 'cs203'] },
+            
+                { name: 'Alexis', grades: [79, 78], courses: ['cs105', 'cs211'] },
+            
+                { name: 'Sam', grades: [91, 82], courses: ['cs445', 'cs303'] },
+            
+                { name: 'Katie', grades: [66, 77], courses: ['cs303', 'cs477'] }
+            
+            ];
+            
+            const result = students.filter(x => x.courses.includes('cs303'))
+                .reduce((total, student) => {
+                    let avgGrade = student.grades.reduce((acc, grad) => {
+                        return acc + grad / student.grades.length
+                    }, 0);
+                    total[student.name] = avgGrade;
+                    return total;
+                }, {})
+                ;
+            console.log(result);
+            
+            // Output: { Quincy: 93.5, Sam: 86.5, Katie: 71.5 }
