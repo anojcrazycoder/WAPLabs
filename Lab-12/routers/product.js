c2fc14dbe51c2fc80290b3e76a6607d5560dd5f0
Anoj Kumar Shrestha <anojbestchoices@gmail.com>
2024-05-15 12:10:26 -0500

    updated the file

--- Lab-12/routers/product.js
+++ Lab-12/routers/product.js
@@ -6,9 +6,6 @@ router.get('',(req,res,next)=>{
     res.sendFile((path.join(__dirname,'..','views','products.html')));
 });
 
-router.get('/add-product',(req,res)=>{
-    res.sendFile((path.join(__dirname,'..','views','productCreate.html')));
-})
 router.post('',express.urlencoded({ extended: true }),(req,res,next)=>{
     products.push(req.body);
     console.log("Product has been added to list");
